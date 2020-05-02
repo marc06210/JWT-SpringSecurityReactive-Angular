da9f66c862a564863bd58a0642327741eca6b262
Marc Guerrini <maguerrini@airfrance.fr>
2020-05-02 08:52:49 +0200

    k8s files first version

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+
+cd $TRAVIS_BUILD_DIR/mlnba-ss
+docker build -t marc06210/mlnba-ss:latest -t marc06210/mlnba-ss:$BUILD_ID -f ./Dockerfile .
+cd $TRAVIS_BUILD_DIR
+ls -l
+echo $PWD
+docker build -t marc06210/mlnba-client:latest -t marc06210/mlnba-client:$BUILD_ID -f ./mlnba-client/Dockerfile ./mlnba-client
+docker push marc06210/mlnba-ss:latest
+docker push mlnba06210/mlnba-client:latest
+
+docker push marc06210/mlnba-ss:$BUILD_ID
+docker push marc06210/mlnba-client:$BUILD_ID
+
+kubectl apply -f ./mlnba-deployment/k8s
+kubectl set image deployments/mlnba-frontend-deployment mlnba-frontend=raghav141988/mlnba-client:$BUILD_ID
+kubectl set image deployments/mlnba-backend-deployment mlnba-api=marc06210/mlnba-ss:$BUILD_ID
