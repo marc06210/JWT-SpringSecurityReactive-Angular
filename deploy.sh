2f1314e706301093bd505644d2680f4f96b62cea
Marc Guerrini <maguerrini@airfrance.fr>
2020-05-03 08:40:46 +0200

    name of docker repo updated

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,22 @@
 echo ">>> starting deploy.sh"
 
 cd $TRAVIS_BUILD_DIR/mlnba-ss
-docker build -t marc06210/mlnba-ss:latest -t marc06210/mlnba-ss:$BUILD_ID -f ./Dockerfile .
+docker build -t elmarco6/mlnba-ss:latest -t elmarco6/mlnba-ss:$BUILD_ID -f ./Dockerfile .
 cd $TRAVIS_BUILD_DIR
 ls -l
 echo ">>> current dir" $PWD
-docker build -t marc06210/mlnba-client:latest -t marc06210/mlnba-client:$BUILD_ID -f ./mlnba-client/Dockerfile ./mlnba-client
+docker build -t elmarco6/mlnba-client:latest -t elmarco6/mlnba-client:$BUILD_ID -f ./mlnba-client/Dockerfile ./mlnba-client
 echo ">>> pushing ss latest"
-docker push marc06210/mlnba-ss:latest
+docker push elmarco6/mlnba-ss:latest
 echo ">>> pushing cs latest"
-docker push marc06210/mlnba-client:latest
+docker push elmarco6/mlnba-client:latest
 
 echo ">>> pushing ss build"
-docker push marc06210/mlnba-ss:$BUILD_ID
+docker push elmarco6/mlnba-ss:$BUILD_ID
 echo ">>> pushing cs build"
-docker push marc06210/mlnba-client:$BUILD_ID
+docker push elmarco6/mlnba-client:$BUILD_ID
 
 echo ">>> applying k8s dep files"
 kubectl apply -f ./mlnba-deployment/k8s
-kubectl set image deployments/mlnba-frontend-deployment mlnba-frontend=raghav141988/mlnba-client:$BUILD_ID
-kubectl set image deployments/mlnba-backend-deployment mlnba-api=marc06210/mlnba-ss:$BUILD_ID
+kubectl set image deployments/mlnba-frontend-deployment mlnba-frontend=elmarco6/mlnba-client:$BUILD_ID
+kubectl set image deployments/mlnba-backend-deployment mlnba-api=elmarco6/mlnba-ss:$BUILD_ID
