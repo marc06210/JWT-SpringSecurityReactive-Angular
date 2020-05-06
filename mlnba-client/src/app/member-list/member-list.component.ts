942601b2b82b6d274b6c11da0f010bbc50336f32
Marc Guerrini <maguerrini@airfrance.fr>
2020-05-06 12:42:49 +0200

    confirmation on member deletion

--- mlnba-client/src/app/member-list/member-list.component.ts
+++ mlnba-client/src/app/member-list/member-list.component.ts
@@ -3,6 +3,9 @@ import { MemberService } from '../shared/member/member.service';
 import { MemberDataSource } from '../shared/member/MemberDataSource';
 import { FormControl } from '@angular/forms';
 import { MatSelect } from '@angular/material/select';
+import { Member } from '../shared/member/member';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../shared/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-member-list',
@@ -21,7 +24,7 @@ export class MemberListComponent implements OnInit {
   displayedColumns: string[] = ['username', 'lastname', 'firstname', 'roles', 'action'];
   dataSource: MemberDataSource;
 
-  constructor(private memberService: MemberService) { }
+  constructor(private memberService: MemberService, public dialog: MatDialog) { }
 
   ngOnInit() {
     this.loadMembers();
@@ -31,16 +34,24 @@ export class MemberListComponent implements OnInit {
     this.unauthorized = false;
     this.memberService.getAll().subscribe(data => {
       this.members = data;
-      //this.dataSource = data;
     });
 
     this.dataSource = new MemberDataSource(this.memberService);
     this.dataSource.loadMembers(this);
   }
 
-  onDeleteRow(row) {
-    console.log('delete: ' + row);
-    this.memberService.delete(row).subscribe(response => this.loadMembers());
+  onDeleteRow(member: Member) {
+    console.log('delete: ' + member.username);
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '350px',
+      data: "Voulez vous vraiment supprimer l'utilisateur " + member.firstname + " " + member.lastname + "?"
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if(result) {
+        this.memberService.delete(member.id).subscribe(response => this.loadMembers());
+      }
+    });
+    
   }
 
 }
