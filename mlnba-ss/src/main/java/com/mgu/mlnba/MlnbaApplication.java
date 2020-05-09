11827f19e96a5882554109f0fbe4882111e5b3b9
Marc Guerrini <maguerrini@airfrance.fr>
2020-05-09 16:15:34 +0200

    missing file of big change

--- mlnba-ss/src/main/java/com/mgu/mlnba/MlnbaApplication.java
+++ mlnba-ss/src/main/java/com/mgu/mlnba/MlnbaApplication.java
@@ -1,8 +1,6 @@
 package com.mgu.mlnba;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -17,148 +15,128 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import com.mgu.mlnba.model.Event;
 import com.mgu.mlnba.model.Member;
 import com.mgu.mlnba.model.Role;
-import com.mgu.mlnba.model.Team;
-import com.mgu.mlnba.model.Training;
 import com.mgu.mlnba.repository.EventRepository;
 import com.mgu.mlnba.repository.MemberRepository;
 import com.mgu.mlnba.repository.RoleRepository;
-import com.mgu.mlnba.repository.TeamRepository;
+import com.mgu.mlnba.repository.TeamGroupRepository;
 
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class MlnbaApplication {
-    
+
     @Autowired
     MemberRepository memberRepo;
-    
+
     @Autowired
     RoleRepository roleRepo;
     @Autowired
-    TeamRepository teamRepo;
+    TeamGroupRepository teamGroupRepo;
     @Autowired
     EventRepository eventRepo;
-    
+
     public static void main(String[] args) {
         SpringApplication.run(MlnbaApplication.class, args);
     }
-    
-//    @Bean
+
+    // @Bean
     CorsWebFilter corsFilter2() {
         return new CorsWebFilter(exchange -> new CorsConfiguration().applyPermitDefaultValues());
     }
-    
-//    @Bean
-    CorsWebFilter corsFilter() {
-            CorsConfiguration config = new CorsConfiguration();
-            config.setAllowCredentials(true);
-            config.addAllowedOrigin("http://localhost:4200");
-            config.addAllowedHeader("*");
-            config.addAllowedMethod("*");
 
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            source.registerCorsConfiguration("/api/team", config);
-            source.registerCorsConfiguration("/api/member", config);
-            
-            return new CorsWebFilter(source);
+    // @Bean
+    CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/team", config);
+        source.registerCorsConfiguration("/api/member", config);
+
+        return new CorsWebFilter(source);
     }
-    
+
 //    @EventListener(ApplicationReadyEvent.class)
     public void list() {
-        teamRepo.findAll()
-            .subscribe(t -> System.out.println("team: " + t.getName()));
-        
-        roleRepo.findAll()
-            .subscribe(t -> System.out.println("role: " +t.getAuthority()));
-        
-        memberRepo.findAll()
-            .subscribe(t -> System.out.println("user: " + t.getUsername()));
+        teamGroupRepo.findAll().subscribe(t -> System.out.println("team: " + t.getId()));
+
+        roleRepo.findAll().subscribe(t -> System.out.println("role: " + t.getAuthority()));
+
+        memberRepo.findAll().subscribe(t -> System.out.println("user: " + t.getUsername()));
     }
     
     @EventListener(ApplicationReadyEvent.class)
+    public void createUser() {
+        memberRepo.deleteAll().block();
+        
+        Member m = new Member();
+        m.setUsername("marc");
+        m.setLastname("guerrini");
+        m.setFirstname("marc");
+        m.setPassword(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("password"));
+
+        roleRepo.findById("role_cto").subscribe(r -> {
+            m.setRoles(Arrays.asList(r));
+            memberRepo.save(m).block();
+        });
+    }
+
+    // @EventListener(ApplicationReadyEvent.class)
     public void createMarc() {
-        
-        Member m2 = memberRepo.findByUsername("marc").block();
-        if(m2==null) {
-            memberRepo.deleteAll().block();
-            roleRepo.deleteAll().block();
-            
-            createRoles();
-            
-            Member m = new Member();
-            m.setUsername("marc");
-            m.setLastname("guerrini");
-            m.setFirstname("marc");
-            m.setPassword(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("password"));
-            
-            roleRepo.findById("role_admin")
-                .subscribe(r -> {
-                    m.setRoles(Arrays.asList(r));
-                    memberRepo.save(m).block();
-                });
-        }
-        System.out.println("Application Ready Event is successfully Started");
-        memberRepo.findAll().subscribe(System.out::println);
-        
+
+        // Team t = new Team();
+        // t.setId("5eb12d86503fee17c9f2ed5a");
+        //
+        // TeamGroup tg = new TeamGroup();
+        // tg.setGender(Gender.FEMALE);
+        // tg.setName("U11");
+        // tg.setDescription("Filles nées en 2009 et 2010");
+        // tg.getTeams().add(t);
+        // teamGroupRepo.save(tg).subscribe();
+        //
+        // Member m2 = memberRepo.findByUsername("marc").block();
+        // if(m2==null) {
+        // memberRepo.deleteAll().block();
+        // roleRepo.deleteAll().block();
+
+        createRoles();
+
+        Member m = new Member();
+        m.setUsername("marc");
+        m.setLastname("guerrini");
+        m.setFirstname("marc");
+        m.setPassword(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("password"));
+
+        roleRepo.findById("role_cto").subscribe(r -> {
+            m.setRoles(Arrays.asList(r));
+            memberRepo.save(m).block();
+        });
+        // }
+        // System.out.println("Application Ready Event is successfully Started");
+        // memberRepo.findAll().subscribe(System.out::println);
     }
-    
+
     public void createRoles() {
-//        Flux.f
-        String[] roles = {"admin", "player", "coach"};
-        Flux.fromArray(roles)
-            .map(r -> new Role("role_"+r))
-            .flatMap(roleRepo::save)
-            .subscribe();
-//            .subscribe();
-//            .subscribe(r -> {
-//                System.out.println("saving: " + r);
-//                roleRepo.save(r).subscribe();
-//            });
-            
+        // Flux.f
+        String[] roles = { "cto", "coach", "player_female", "player_male", "otm", "referee" };
+        Flux.fromArray(roles).map(r -> new Role("role_" + r)).flatMap(roleRepo::save).subscribe();
     }
-//    @EventListener(ApplicationReadyEvent.class)
+
+    // @EventListener(ApplicationReadyEvent.class)
     public void createMatch() {
         eventRepo.deleteAll().block();
-        
-      Flux.range(0, 10)
-          .map(i -> {
-              Event e = new Event();
-              e.setTitle("Match " + i);
-              e.setDescription("match " + i);
-              e.setOpponent("opponent " + i);
-              return e;
-          })
-          .flatMap(eventRepo::save)
-          .subscribe();
-  }
-    //@EventListener(ApplicationReadyEvent.class)
-    public void createTeam() {
-        List<Training> trainings = new ArrayList<>();
-        Training tr = new Training();
-        tr.setDay("Mardi");
-        tr.setFromTime("17h15");
-        tr.setToTime("18h45");
-        trainings.add(tr);
-        tr = new Training();
-        tr.setDay("Jeudi");
-        tr.setFromTime("17h30");
-        tr.setToTime("19h00");
-        trainings.add(tr);
-        
-        Team t = new Team();
-        t.setName("U11F");
-        t.setDescription("Filles nées en 2009 et 2010");
-        t.setTrainings(trainings);
-        
-        teamRepo.save(t).subscribe();
-  }
-    
-//    @EventListener(ApplicationReadyEvent.class)
-    public void deleteAll() {
-        eventRepo.deleteAll().block();
-        memberRepo.deleteAll().block();
-        roleRepo.deleteAll().block();
-        teamRepo.deleteAll().block();
+
+        Flux.range(0, 10).map(i -> {
+            Event e = new Event();
+            e.setTitle("Match " + i);
+            e.setDescription("match " + i);
+            e.setOpponent("opponent " + i);
+            return e;
+        }).flatMap(eventRepo::save).subscribe();
     }
+    
 
 }
